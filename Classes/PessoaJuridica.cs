61ec71f05ed7f531fb2beec72760189ded8a0d8c
Luhan Melo <luhanalves@outlook.com>
2022-08-01 20:58:25 -0300

    Atividade de Validação Encontro Remoto 4

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,4 +1,5 @@
 using UC12_ER2.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace UC12_ER2.Classes
 {
@@ -16,9 +17,32 @@ namespace UC12_ER2.Classes
             throw new NotImplementedException();
         }
 
+        /*
+            XX.XXX.XXX/0001-XX
+            \d{2}
+        */
+
+
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            if(Regex.IsMatch(cnpj,@"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if(cnpj.Length == 18) 
+                {
+                    if(cnpj.Substring(11,4) == "0001") // ele vai iniciar no caracter 11 e pegar os próximos 4
+                    {
+                        return true;
+                    }
+                }
+                else if(cnpj.Length == 14)
+                {
+                    if(cnpj.Substring(8,4) == "0001") //ele vai iniciar no caractere 8 e pegar os próximos 4
+                    {
+                        return true;
+                    }
+                }
+            }
+           return false;            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using UC12_ER2.Classes;
 
+/*
 PessoaFisica novaPf = new PessoaFisica();
 Endereco novoEnd = new Endereco();
 
 PessoaFisica metodoPf = new PessoaFisica();
+
 
 novaPf.nome = "Luiz";
 novaPf.dataNascimento = "18/02/1984";
@@ -23,7 +26,7 @@ Nome: {novaPf.nome}
 Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}
 Maior de idade: {metodoPf.ValidarDataNascimento(novaPf.dataNascimento)}
 ");
-
+*/
 
 
 
@@ -40,3 +43,22 @@ Console.WriteLine($"Nome: {novaPf.nome}");
 
 // Console.WriteLine(novaPf.ValidarDataNascimento("01/01/2000"));
 
+PessoaJuridica metodoPj = new PessoaJuridica();
+PessoaJuridica novaPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.nome = "NomePj";
+novaPj.cnpj = "00000000000100";
+novaPj.razaosocial = "Razao Social Pj";
+
+novoEndPj.logradouro = "Alameda Barao de Limeira";
+novoEndPj.numero = 539;
+novoEndPj.complemento = "Senai Informatica" ;
+novoEndPj.endComercial = true;
+novaPj.endereco = novoEndPj;
+
+Console.WriteLine(@$"
+Nome: {novaPj.nome}
+Razao social: {novaPj.razaosocial}
+CNPJ: {novaPj.cnpj}
+CNPJ é válido: {metodoPj.ValidarCnpj(novaPj.cnpj)}");
